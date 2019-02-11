562bc331feb12b436b7c1a34054b5981510a51b6
stv <stv@stanford.edu>
2019-02-10 21:01:39 -0800

    bootstrap

--- Bootstrap1.sh
+++ Bootstrap1.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-whoami
 apt-get update -y
 apt-get install -y build-essential
 apt-get install -y python-dev python-pip
+apt-get install -y virtualenv
 # apt-get install -y python3-dev python3-pip
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
 apt-get install -y nodejs
@@ -10,14 +10,5 @@ apt-get install -y npm
 npm install -g eslint
 npm install -g less
 npm install -g csslint
-sudo -H -u vagrant git clone https://github.com/edx/xblock-sdk.git /home/vagrant/sdk
-cd /home/vagrant/sdk
-sed -i.bak "s/'[_a-z]\+ *= *sample_xblocks\.\(basic\.\(problem\|content\|slider\)\|.*thumbs\)/\# &/g" setup.py
-sed -i.bak 's/.*acid-block\.git/# &/g' requirements/dev.txt
-pip install --upgrade pip
-hash pip
-pip install tox
-pip install -e /home/vagrant/xblock/
-pip install -e .
-pip install -qr ./requirements/local.txt --exists-action w
-pip install -qr ./requirements/dev.txt --exists-action w
+# pip install --upgrade pip
+# hash pip
--- Bootstrap2.sh
+++ Bootstrap2.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+sudo -H -u vagrant git clone https://github.com/edx/xblock-sdk.git /home/vagrant/sdk
 cd /home/vagrant/sdk
-./manage.py migrate
+sed -i.bak "s/'[_a-z]\+ *= *sample_xblocks\.\(basic\.\(problem\|content\|slider\)\|.*thumbs\)/\# &/g" setup.py
+sed -i.bak 's/.*acid-block\.git/# &/g' requirements/dev.txt
+virtualenv /home/vagrant/venv
+. /home/vagrant/venv/bin/activate
+pip install tox
+pip install -e /home/vagrant/xblock/
+pip install -e .
+pip install -qr ./requirements/local.txt --exists-action w
+pip install -qr ./requirements/dev.txt --exists-action w
+python ./manage.py migrate
