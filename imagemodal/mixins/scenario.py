5065a5cf7fed79aa4f2cd676e53c994eb0830a32
stv <stv@stanford.edu>
2019-03-11 18:20:08 -0700

    Split scenarios out into XML files

--- imagemodal/mixins/scenario.py
+++ imagemodal/mixins/scenario.py
@@ -1,42 +1,72 @@
 """
 Mixin workbench behavior into XBlocks
 """
+from glob import glob
+import pkg_resources
 
 
-class ImageModalScenarioMixin(object):
+def _read_file(file_path):
+    """
+    Read in a file's contents
+    """
+    with open(file_path) as file_input:
+        file_contents = file_input.read()
+    return file_contents
+
+
+def _parse_title(file_path):
+    """
+    Parse a title from a file name
+    """
+    title = file_path
+    title = title.split('/')[-1]
+    title = '.'.join(title.split('.')[:-1])
+    title = ' '.join(title.split('-'))
+    title = ' '.join([
+        word.capitalize()
+        for word in title.split(' ')
+    ])
+    return title
+
+
+def _read_files(files):
+    """
+    Read the contents of a list of files
+    """
+    file_contents = [
+        (
+            _parse_title(file_path),
+            _read_file(file_path),
+        )
+        for file_path in files
+    ]
+    return file_contents
+
+
+def _find_files(directory):
+    """
+    Find XML files in the directory
+    """
+    pattern = "{directory}/*.xml".format(
+        directory=directory,
+    )
+    files = glob(pattern)
+    return files
+
+
+class XBlockWorkbenchMixin(object):
     """
     Provide a default test workbench for the XBlock
     """
 
-    @staticmethod
-    def workbench_scenarios():
+    @classmethod
+    def workbench_scenarios(cls):
         """
         Gather scenarios to be displayed in the workbench
         """
-        return [
-            ('Image Modal XBlock, single',
-             """<sequence_demo>
-                    <imagemodal
-                        display_name="Image Modal With Thumbnail"
-                        thumbnail_url="http://upload.wikimedia.org/wikipedia/commons/thumb/4/48/1853_Kaei_6_Japanese_Map_of_the_World_-_Geographicus_-_ChikyuBankokuHozu-nakajima-1853.jpg/640px-1853_Kaei_6_Japanese_Map_of_the_World_-_Geographicus_-_ChikyuBankokuHozu-nakajima-1853.jpg"
-                        description="Put screenreader text here"
-                    />
-                </sequence_demo>
-             """),
-            ('Image Modal XBlock, multiple',
-             """<sequence_demo>
-                    <vertical_demo>
-                        <imagemodal
-                            display_name="Image Modal With Thumbnail"
-                            thumbnail_url="http://upload.wikimedia.org/wikipedia/commons/thumb/4/48/1853_Kaei_6_Japanese_Map_of_the_World_-_Geographicus_-_ChikyuBankokuHozu-nakajima-1853.jpg/640px-1853_Kaei_6_Japanese_Map_of_the_World_-_Geographicus_-_ChikyuBankokuHozu-nakajima-1853.jpg"
-                            description="Put screenreader text here"
-                        />
-                        <imagemodal description="Write stuff here" />
-                    </vertical_demo>
-                    <vertical_demo>
-                        <imagemodal description="Write stuff here" />
-                        <imagemodal description="Write more here" />
-                    </vertical_demo>
-                </sequence_demo>
-             """),
-        ]
+        module = cls.__module__
+        module = module.split('.')[0]
+        directory = pkg_resources.resource_filename(module, 'scenarios')
+        files = _find_files(directory)
+        scenarios = _read_files(files)
+        return scenarios
